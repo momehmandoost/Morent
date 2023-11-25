dddb1f4f4edf0b3a7b0ccbfe7bf665f1125b22c7
momehmandoost <mehmandoost2@gmail.com>
2023-11-25 04:03:13 +0330

    4th commit

--- src/Pages/Models.jsx
+++ src/Pages/Models.jsx
@@ -1,15 +1,6 @@
 import Footer from "../components/Footer";
 import HeroPages from "../components/HeroPages";
-import Star from "../images/cars-big/star.svg";
-import CarImg1 from "../images/cars-big/audi-box.png";
 
-import CarImg2 from "../images/cars-big/golf6-box.png";
-import CarImg3 from "../images/cars-big/toyota-box.png";
-import CarImg4 from "../images/cars-big/bmw-box.png";
-import CarImg5 from "../images/cars-big/benz-box.png";
-import CarImg6 from "../images/cars-big/passat-box.png";
-
-import { Link } from "react-router-dom";
 import {
   IconCar,
   IconPhone,
@@ -18,7 +9,7 @@ import {
   IconDashboard,
   IconChartRadar,
 } from "@tabler/icons-react";
-import CarModels from "../components/CarModels";
+
 import { CarModelsBox } from "../components/CarModelsBox";
 
 function Models() {
